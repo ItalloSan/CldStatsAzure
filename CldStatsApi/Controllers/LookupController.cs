9c6c003136b2ea2817092f5247ae013710ec8b84
Stephen D'Agostino <stephen@itallosan.co.uk>
2021-09-14 16:03:08 +0100

    work in progress

--- CldStatsApi/Controllers/LookupController.cs
+++ CldStatsApi/Controllers/LookupController.cs
@@ -2,27 +2,19 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CldServiceFactory.Services.Interfaces;
-using CldStatsData;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+using CldStatsDto.Dto.Commands;
 
 namespace CldStatsApi.Controllers
 {
     public class LookupController : Controller
     {
         private readonly ILookupTablesService _lookupTablesService;
-        //private readonly CldStatsDbContext _cldStatsDbContext;
 
         public LookupController(
             ILookupTablesService lookupTablesService
-            //,CldStatsDbContext cldStatsDbContext
-            // 
-            //,ILogger logger
             )
         {
-            _lookupTablesService = lookupTablesService;
-            //_cldStatsDbContext = cldStatsDbContext;
-            //_logger = logger;
+            _lookupTablesService = lookupTablesService;   
         }
 
         [HttpGet]
@@ -36,7 +28,25 @@ namespace CldStatsApi.Controllers
             }
             catch (Exception e)
             {
-                //_logger.LogError(e.Message, e);
+                return BadRequest(e.Message);
+            }
+        }
+        /// <summary>
+        /// Find the lookup tables depending on what has been selected
+        /// </summary>
+        /// <param name="findLookupTables"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("FindLookupTables")]
+        public async Task<IActionResult> FindLookupTables(FindLookupTables findLookupTables)
+        {
+            try
+            {
+                var lookLookupTablesDtos = await _lookupTablesService.GetLookupTables();
+                return Ok(lookLookupTablesDtos);
+            }
+            catch (Exception e)
+            {
                 return BadRequest(e.Message);
             }
         }
---
+++ CldStatsDto/Dto/Commands/FindLookupTables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CldStatsDto.Dto.Commands
+{
+    public class FindLookupTables
+    {
+        public List<ClusterDto> Clusters { get; set; }
+    }
+}
